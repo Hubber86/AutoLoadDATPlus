641c2851ce3eda85ef11f037476c992277dfab0d
Hubber86 <110298413+Hubber86@users.noreply.github.com>
2025-07-11 17:27:02 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
+# __init__.py
+
 def classFactory(iface):
+    """
+    Load AutoLoadDATPlusPlugin class from file and return an instance to QGIS.
+
+    :param iface: A QGIS interface instance (QgisInterface)
+    :return: An instance of the plugin
+    """
     from .AutoLoadDATPlus import AutoLoadDATPlusPlugin
     return AutoLoadDATPlusPlugin(iface)
